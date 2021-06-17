b0c36fdc95fe05da23468e6bc2b95d0a1ab9f17a
lachero-work <asaf@gwumkt.com>
2021-06-17 10:49:29 +0300

    removed dbService and use directly data access layer

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,2 @@
 export { default as EventService } from './events/EventService.js';
-export { default as DbService } from './database/DbService.js';
 export { default as AuthService } from './auth/AuthService.js';
--- src/shared/index.js
+++ src/shared/index.js
@@ -1,1 +1,2 @@
 export { default as BaseSchema } from './BaseSchema.js';
+export { default as AppError } from './AppError.js';
